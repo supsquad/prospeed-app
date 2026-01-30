b88e84157649cdc8cd5e3114846cb1d96428fca6
cozimizo <cozimizo@gmail.com>
2026-01-30 22:11:05 +0700

    update: no console popup

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+# Fix for --windows-disable-console: redirect stdout/stderr to devnull
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, 'w')
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, 'w')
+
 import tkinter as tk
 
 from speed import ProxySpeedTester

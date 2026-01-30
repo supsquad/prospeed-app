e72272ce958c2bbf50c219a2385abff547e83d3b
cozimizo <cozimizo@gmail.com>
2026-01-30 21:33:37 +0700

    update: build script

--- script/build.bat
+++ script/build.bat
@@ -2,4 +2,4 @@
 
 for /F %%i in (.env.prod) do set %%i
 
-python -m nuitka --standalone --onefile --enable-plugin=tk-inter --output-dir=build --output-filename=prospeed-v%1.exe app/main.py
+python -m nuitka --standalone --onefile --enable-plugin=tk-inter --windows-disable-console --output-dir=build --output-filename=prospeed-v%1.exe app/main.py
